8688cf0a570b3dd4d4129f3f08e64ea8bef096ba
73N37 <x0d4r4p@proton.me>
2025-10-23 13:49:51 +0200

    created method definnition of getBookById(int) and getBookByByName(String)

--- src/main/java/T3N3T/User/UserEntity.java
+++ src/main/java/T3N3T/User/UserEntity.java
@@ -1,15 +1,19 @@
 package T3N3T.User;
 
+@lombok.AllArgsConstructor
+@lombok.RequiredArgsConstructor
+@lombok.Setter
 @lombok.Getter
 public class
 UserEntity
 {
     @jakarta.persistence.Id
     @jakarta.persistence.GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
-    private java.lang.Long      userId;
-    private T3N3T.Enum.Role     role;
-    private java.lang.String    username;
-
+    private java.lang.Long                          userId;
+    private T3N3T.Enum.Role                         role;
+    private java.lang.String                        username;
+    private java.lang.String                        hashedPassword;
+    private java.lang.Set<T3N3T.Book.BookEntity>    books;
 
     public UserEntity(T3N3T.Enum.Role     role,
                       java.lang.String    username)
@@ -17,4 +21,11 @@ UserEntity
         this.role       = role;
         this.username   = username;
     }
+
+    BookEntity getBookByTitle(java.lang.String title)
+    {
+
+    }
+
+    BookEntity getBookById(java.lang)
 }
